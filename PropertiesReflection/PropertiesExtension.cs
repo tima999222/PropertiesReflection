c06cc90932cd500ed1b1523cd61ece4f4d7a2f9f
Тимофей <tima.antya@mail.ru>
2023-05-10 15:02:30 +0300

    Добавьте файлы проекта.

---
+++ PropertiesReflection/PropertiesExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PropertiesReflection
+{
+    public static class PropertiesExtension
+    {
+        public static List<PropertyInfo> GetProperties<T>(this T obj)
+        {
+            Type type = obj.GetType();
+            var properties = type.GetProperties();
+
+            return properties.ToList();
+        }
+    }
+}
